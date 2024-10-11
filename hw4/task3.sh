9d71de1f068aaff514a38201d8bd0d0d055c36bf
Shruti-ch-git <jayashrutichintalapati@gmail.com>
2024-10-10 21:26:29 -0400

    Create task3.sh

---
+++ hw4/task3.sh
@@ -0,0 +1,15 @@
+gawk -F, '$3 == 2 && $13 ~ /S/ {print $0}' /Users/shrutichintalapati/Downloads/hw4/titanic.csv | \
+sed 's/female/F/g; s/male/M/g' | \
+tee /dev/tty | \
+gawk -F, '{
+    if ($7 != "") { 
+        sum += $7; 
+        count++; 
+    } 
+} 
+END {
+    if (count > 0) 
+        print "Average Age:", sum / count; 
+    else 
+        print "No age present.";
+}'
